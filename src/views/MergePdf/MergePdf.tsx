2445a857c0b821ab63e77c6ab5359a1b845b48bb
Fahad Ahmed <fahad.ahmed@me.com>
2024-08-12 18:13:41 +0800

    #1 setup the project with astro and client side and server actions

--- src/views/MergePdf/MergePdf.tsx
+++ src/views/MergePdf/MergePdf.tsx
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+
 export default function MergePdf() {
+  const [pdfFiles, setPdfFiles] = useState<File[]>([]);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      setPdfFiles(Array.from(event.target.files))
+    }
+  };
+
+  console.log("We are showing client side updated", pdfFiles)
   return (
     <div>
       <h1>Merge PDF</h1>
+      <input type="file" multiple accept='application/pdf' onChange={handleFileChange} />
     </div>
   )
 }
