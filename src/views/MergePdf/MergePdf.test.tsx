6d1b925b6a103992059bacfba7eb56de3dbaae0a
Fahad Ahmed <fahad.ahmed@me.com>
2024-08-16 08:02:37 +0530

    #1 - removing unused packages in test file

--- src/views/MergePdf/MergePdf.test.tsx
+++ src/views/MergePdf/MergePdf.test.tsx
@@ -1,8 +1,8 @@
 // src/components/MergePdf.test.tsx
-import React from 'react';
+// import React from 'react';
 import { describe, it, expect } from 'vitest';
-import { render, screen, fireEvent } from '@testing-library/react';
-import MergePdf from './MergePdf';
+// import { render, screen, fireEvent } from '@testing-library/react';
+// import MergePdf from './MergePdf';
 
 const add = (a: number, b: number) => a + b;
 describe('MergePdf Component', () => {
