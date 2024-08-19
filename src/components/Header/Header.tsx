7436f7172fdcaf2d48d7cb0e417f9d9051913290
Fahad Ahmed <fahad.ahmed@me.com>
2024-08-19 17:17:17 +0530

    #2 - created base Header and Button components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,19 @@
+import { Button } from '..';
+import './header.css';
+
 export default function Header() {
   return (
-    <div>Header component</div>
+    <div className="header">
+      <div className="logo">PDF Editor</div>
+      <div className="navigation">
+        <div className="links">
+          <a href="">Tools</a>
+        </div>
+        <div className="actions">
+          <Button type="button" label="Dashboard" />
+          <Button type="button" label="Logout" />
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
+export { default as Button } from './Button';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 
+export * from './Button';
 export * from './Footer';
 export * from './Header';
