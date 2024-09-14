29a093ca924a07361062cdaca002d85c521431ed
Fahad Ahmed <fahad.ahmed@me.com>
2024-09-14 21:56:38 +1000

    #16 setting up register and signin routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [react()],
   test: {
     environment: 'jsdom',
--- src/env.d.ts
+++ src/env.d.ts
@@ -2,15 +2,12 @@
 /// <reference types="astro/client" />
 
 interface ImportMetaEnv {
-  readonly FIREBASE_PRIVATE_KEY_ID: string;
-  readonly FIREBASE_PRIVATE_KEY: string;
-  readonly FIREBASE_PROJECT_ID: string;
-  readonly FIREBASE_CLIENT_EMAIL: string;
-  readonly FIREBASE_CLIENT_ID: string;
-  readonly FIREBASE_AUTH_URI: string;
-  readonly FIREBASE_TOKEN_URI: string;
-  readonly FIREBASE_AUTH_CERT_URL: string;
-  readonly FIREBASE_CLIENT_CERT_URL: string;
+  readonly PUBLIC_FIREBASE_API_KEY: string;
+  readonly PUBLIC_FIREBASE_AUTH_DOMAIN: string;
+  readonly PUBLIC_FIREBASE_STORAGE_BUCKET: string;
+  readonly PUBLIC_FIREBASE_MESSENGER_ID: string;
+  readonly PUBLIC_FIREBASE_APP_ID: string;
+  readonly PUBLIC_FIREBASE_MEASUREMENT_ID: string;
 }
 
 interface ImportMeta {
